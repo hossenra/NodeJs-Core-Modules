ef33b05871a358744ef9ca409f0a8f585b84a37e
RAZIBUL HOSSEN <razibul.hossen@photovoltaikvision.de>
2025-11-23 15:03:18 +0100

    data added

--- output/hashing.js
+++ output/hashing.js
@@ -2,6 +2,21 @@
 const crypto = require("crypto");
 
 console.log("\n MD5 Hash: ");
-const md5Hash = crypto.createHash("md5").update("password123").digest("hex");
+const md5Hash = crypto.createHash("md5").update("password123").digest("hex"); //* Not Recommended
 console.log("input : password123");
 console.log("MD5 HashedPassword : ", md5Hash);
+console.log("MD5 hash2 : ", md5Hash);
+
+const sha256Hash = crypto
+  .createHash("sha256")
+  .update("password123")
+  .digest("hex");
+console.log("input : password123");
+console.log("SHA256 HashedPassword : ", sha256Hash);
+
+const sha512Hash = crypto
+  .createHash("sha512")
+  .update("password123")
+  .digest("hex");
+console.log("input : password123");
+console.log("SHA512 HashedPassword : ", sha512Hash);
